e8a70d0050e2105e638d711675332224febfceeb
Duy Huynh <humindu312@gmail.com>
2022-01-12 17:05:33 +0700

    add components

--- src/App.js
+++ src/App.js
@@ -1,41 +1,12 @@
+import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import TodoFeture from './features/Todo/pages';
 
 function App() {
-  const name = 'Duy';
-  const age = 18;
-  const isMale = true;
-  const student = {
-    name: 'Easy Frontend'
-  };
-  const colorList = ['red', 'green', 'blue']
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Duy Huynh
-        </p>
-        <p>Xin chao {name} - {age} - {isMale ? 'Male' : 'Female'} </p>
-
-        {isMale && (
-          <div>
-            <p>Male 1</p>
-            <p>Male 2</p>
-            <p>Male 3</p>
-          </div>
-        )}
-
-        <p>{student.name}</p>
-
-        <ul>
-          {colorList.map(color => (
-            <li style={{ color }}>{color}</li>
-          ))}
-        </ul>
-      </header>
-    </div>
-  );
+  <div className="App">
+    <TodoFeture />
+  </div>
 }
 
 export default App;
