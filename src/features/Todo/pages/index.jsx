b8cd6fd2cc7ee3433062b89dc09e458ec240175c
Duy Huynh <humindu312@gmail.com>
2022-01-12 16:46:34 +0700

    commit on 'master'

---
+++ src/features/Todo/pages/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+TodoFeture.propTypes = {
+
+};
+
+function TodoFeture(props) {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default TodoFeture;
