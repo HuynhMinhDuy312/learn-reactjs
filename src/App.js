3994e14ec0700825e74daee8fd4b5e4bf92a57d1
Duy Huynh <humindu312@gmail.com>
2022-01-22 16:50:01 +0700

    API Lesson

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link, NavLink, Redirect, Route, Switch } from 'react-router-dom';
 import TodoFeture from './features/Todo/pages';
-import AlbumFeature from './features/Album/pages'
+import AlbumFeature from './features/Album/pages';
 import NotFound from './components/NotFound';
+import productApi from './api/productApi';
 
 function App() {
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const params = {
+        _limit: 10,
+      };
+      const productList = await productApi.getAll(params);
+      console.log(productList);
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <div className="App">
       Home Page
-      <p><NavLink to="/todos" activeClassName='active-menu'>Todos</NavLink></p>
-      <p><NavLink to="/album" activeClassName='active'>Album</NavLink></p>
+      <p>
+        <NavLink to="/todos" activeClassName="active-menu">
+          Todos
+        </NavLink>
+      </p>
+      <p>
+        <NavLink to="/album" activeClassName="active">
+          Album
+        </NavLink>
+      </p>
       <Switch>
         <Redirect from="/home" to="/" />
         <Redirect from="/post-list/:postId" to="/posts/:postId" />
